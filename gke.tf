d07d81b1bf1b34b895c92e74aa867383b64d1ed7
Wil Seely-Kirk <wil.seely-kirk@sap.com>
2017-09-14 10:58:06 -0700

    updated gke.tf

--- gke.tf
+++ gke.tf
@@ -4,30 +4,20 @@ provider "google" {
   region      = "${var.region}"
 }
 
-resource "google_compute_instance" "default" {
+resource "google_container_cluster" "primary" {
   name           = "terraform-test"
-  machine_type   = "g1-small"
   zone		 = "us-west1-a"
-  boot_disk {
-    initialize_params {
-      image = "coreos-stable-1409-9-0-v20170814"
-      }
-  }
-  network_interface {
-    network = "default"
-    access_config {
-     nat_ip = "${google_compute_address.default.address}"
+  initial_node_count = 3
+  node_config {
+    machine_type = "n1-standard-1"
+    labels {
+      cluster = "terraform"
     }
-  }
+    tags {
+      ["terraform", "blah"]
+    }
+
+output "cluster_id" {
+ value = "${google_container_cluster.primary.self_link}"
 }
 
-resource "google_compute_address" "default" {
-  name = "terraform-ip"
-}
-
-output "instance_id" {
- value = "${google_compute_instance.default.self_link}"
-}
-output "instance_IP" {
- value = "${google_compute_address.default.default.network_interface.0.access_config.0.nat_ip}"
-}
