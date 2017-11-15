c0e5ae73b28dec1f61bb73e654806e6c6af0cf5b
Wil Seely-Kirk <wil.seely-kirk@sap.com>
2017-11-15 15:03:46 -0800

    removed extra directories

--- cluster/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "project" {}
-variable "region" {}
-variable "zone" {}
-variable "node_count" {}
-variable "cluster_name" {}
-variable "node_version" {}
-variable "min_nodes" {}
-variable "max_nodes" {}
