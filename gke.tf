3a7386e351caa76f695011fe67dcc618cdffeb4d
Wil Seely-Kirk <wil.seely-kirk@sap.com>
2017-11-14 17:13:06 -0800

    added autoscaling node pool

--- gke.tf
+++ gke.tf
@@ -14,14 +14,33 @@ terraform {
 data "google_container_engine_versions" "uswest1a" {
   zone = "us-west1-a"
 }
+
+resource "google_container_node_pool" "np" {
+  name = "${var.cluster_name}-node-pool"
+  zone = "${var.zone}"
+  cluster = "${google_container_cluster.primary.name}"
+  initial_node_count = "${var.node_count}"
+  autoscaling {
+    min_node_count = "${var.min_nodes}"
+    max_node_count = "${var.max_nodes}"
+  }
+}
+
 resource "google_container_cluster" "primary" {
-  name           = "${var.cluster_name}"
-  zone		 = "us-west1-a"
+  name     = "${var.cluster_name}"
+  zone		 = "${var.zone}"
+#  node_pool = ["${google_container_node_pool.np.name}"]
   initial_node_count = "${var.node_count}"
 #  node_version = "${data.google_container_engine_versions.uswest1a.latest_node_version}"
   node_version = "${var.node_version}"
   node_config {
     machine_type = "n1-standard-1"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
     labels {
       cluster = "terraform"
     }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,8 @@
 variable "project" {}
 variable "region" {}
+variable "zone" {}
 variable "node_count" {}
 variable "cluster_name" {}
 variable "node_version" {}
+variable "min_nodes" {}
+variable "max_nodes" {}
